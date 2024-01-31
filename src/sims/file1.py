c2c887d59516e243eb0cfa9763cbd06fb2fc7f1c
Mark Orr <mo6xj@virginia.edu>
2024-01-30 20:04:43 -0500

    std

--- src/sims/file1.py
+++ src/sims/file1.py
@@ -25,8 +25,65 @@ csvs from sims in sim blocks.
 real_sim_endstates = ibl.extract_endstates(f'{data_in_dir}Obama.csv')
 real_dist_endstates = ibl.dist_of_endstates(real_sim_endstates)
 plt.style.use('fivethirtyeight')
-plt.plot(ibl.dist_of_endstates(real_sim_endstates))
+fig, axes = plt.subplots(1,1,figsize=(5,5),sharex=True,sharey=True)
+axes.plot(ibl.dist_of_endstates(real_sim_endstates))
+axes.set_ylabel('Frequency', labelpad=3,size=10)
+axes.set_xlabel('Retrieval Dec. Index', labelpad=3,size=10)
+axes.tick_params(axis='x', labelsize=5)
+axes.tick_params(axis='y', labelsize=7)
+
+#axes.set_xticks([10,1013])
+#axes.set_xticklabels(labels=['10','1013'],rotation=45)
+axes.set_xticks([0,10,100,500,1000,1023])
+axes.set_xticklabels(labels=[0,10,100,500,1000,1023],rotation=60)
 plt.savefig(f'{graph_out_dir}Fig_Gibbs_RealData.png',dpi=400,bbox_inches='tight')
+
+'''COMBINE INTO ONE PLOT'''
+
+
+
+axes[0,0].set_ylabel('Frequency (IBL)', labelpad=10,size=10)
+axes[1,0].set_ylabel('Frequency (CAN)', labelpad=10,size=10)
+axes[1,0].set_xlabel('Retrieval Dec. Index', labelpad=10,size=6)
+axes[1,1].set_xlabel('Retrieval Dec. Index', labelpad=10,size=6)
+axes[1,2].set_xlabel('Retrieval Dec. Index', labelpad=10,size=6)
+axes[1,3].set_xlabel('Retrieval Dec. Index', labelpad=10,size=6)
+
+axes[1,0].tick_params(axis='x', labelsize=5)
+axes[1,1].tick_params(axis='x', labelsize=5)
+axes[1,2].tick_params(axis='x', labelsize=5)
+axes[1,3].tick_params(axis='x', labelsize=5)
+axes[0,0].tick_params(axis='y', labelsize=7)
+axes[1,0].tick_params(axis='y', labelsize=7)
+axes[1,0].set_xticks([10,1013])
+axes[1,0].set_xticklabels(labels=['10','1013'])
+
+#CAN
+noise_list = [9,2,1,.125]
+noise_list_invert = [.11,.5,1,8]
+panel_list = ['A*','B*','C*','D*']
+counter = 0
+
+for x in range(1,2):
+    for y in range(0,4):
+        ns_fn = noise_list[counter]
+        sim_name = f'can_beta_{ns_fn}'
+        sim_endstates = ibl.extract_endstates(f'{data_out_dir}{sim_name}.csv')
+        dist_endstates = ibl.dist_of_endstates(sim_endstates)
+        axes[x,y].plot(ibl.dist_of_endstates(sim_endstates))
+        axes[x,y].set_ylim(0,100)
+        axes[x,y].text(20,85,f'Noise = {noise_list_invert[counter]}',fontsize=5)
+        axes[x,y].text(850,95,panel_list[counter],fontsize=7)
+        counter = counter+1
+        #print(x,' ',y,' ',counter)
+
+plt.subplots_adjust(wspace=.2,hspace=.2,left=0.15,bottom=0.15,right=0.90,top=0.90,)
+plt.savefig(f'{graph_out_dir}CAN_Gibbs_2X4.png',dpi=300,bbox_inches='tight')
+#plt.show()
+#EOF
+
+
+
 
 
 '''SIM RUNS'''
